acd369cc14188806ab5126027188d7af115ce444
Ubuntu <ubuntu@ip-172-31-36-41.ap-south-1.compute.internal>
2024-01-15 11:42:29 +0000

    first commit

---
+++ area.java
@@ -0,0 +1,16 @@
+package apna;
+import java.util.*;
+public class area {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the radious of circle ");
+		Scanner sc = new Scanner(System.in);
+		int radius = sc.nextInt();
+		double Area = (3.14 * (radius*radius));
+		System.out.println("Area of circle ="+ Area);
+		
+		
+
+	}
+
+}
