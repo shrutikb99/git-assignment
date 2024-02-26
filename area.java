8b50eb62ad180dafed45ba9fd23eb63dca56dc11
shrutikb99 <154527573+shrutikb99@users.noreply.github.com>
2024-02-26 15:47:14 +0530

    Update area.java

--- area.java
+++ area.java
@@ -1,13 +1,9 @@
 package apna;
 import java.util.*;
-public class area {
+public class hello {
 
 	public static void main(String[] args) {
-		System.out.println("Enter the radious of circle ");
-		Scanner sc = new Scanner(System.in);
-		int radius = sc.nextInt();
-		double Area = (3.14 * (radius*radius));
-		System.out.println("Area of circle ="+ Area);
+		System.out.println("Hello Welcome to jenkins");
 		
 		
 
